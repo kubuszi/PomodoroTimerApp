58662284dead008f0824454c9e3fa25ca2e27dcb
kubuszi <kubushii2@wp.pl>
2024-05-07 11:33:46 +0200

    Correct seconds countdown

--- main.py
+++ main.py
@@ -16,12 +16,14 @@ window.config(padx=100, pady=50, bg=YELLOW)
 
 
 def start_timer():
-    count_down(5  * 60)
+    count_down(5 * 3)
 
 
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
+    if count_sec < 10:
+        count_sec = f"0{count_sec}"
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
